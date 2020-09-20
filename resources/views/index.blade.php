9855657ae7e9b72aaddfb7c3ab771ba0c0bc424b
Sebastian Scheel Edelmann <sebastian@edelmann.co.uk>
2020-09-20 18:36:04 +0200

    prices off

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -364,33 +364,7 @@
         </section>
         <!-- /Section: Contact -->
 
-        <!-- Footer -->
-        <footer class="footer">
-
-            <div class="row">
-                <div class="col-12 text-center">
-
-                    <div class="text-muted mb-2">
-                        &copy; 2016-2020 <a href="https://1.envato.market/rWBad" target="_blank">viui18.com</a>
-                    </div>
-
-                    <div>
-                        <a href="https://www.facebook.com/viui18com/" target="_blank" class="btn">
-                            <i class="fa fa-facebook fs-1"></i>
-                        </a>
-                        <a href="https://www.facebook.com/viui18com/" target="_blank" class="btn">
-                            <i class="fa fa-instagram fs-1"></i>
-                        </a>
-                        <a href="https://www.pinterest.ru/viui18/" target="_blank" class="btn">
-                            <i class="fa fa-pinterest-p fs-1"></i>
-                        </a>
-                    </div>
-
-                </div>
-            </div>
-
-        </footer>
-        <!-- /Footer -->
+        @include('layouts.footer')
 
     </main>
 @endsection
